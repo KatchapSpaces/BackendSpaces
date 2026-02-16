b82574724f5ee290b553b900a152e18a86ce8231
Muhammad Umar Hassan <your_email@example.com>
2026-02-16 17:25:38 +0500

    update

--- config/cors.php
+++ config/cors.php
@@ -10,13 +10,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-         'https://katchap.com',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5174',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_headers' => ['*'],
 
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,31 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\ResetPasswordController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Root route (homepage) - temporary test page
+Route::get('/', function () {
+    return view('welcome'); // Make sure resources/views/welcome.blade.php exists
+    // OR for a simple test message, use:
+    // return 'Laravel dev server is running!';
+});
+
+// Floorplan files route
 Route::get('/floorplan/{path}', function ($path) {
     $fullPath = storage_path('app/public/' . $path);
     if (!file_exists($fullPath)) abort(404, "File not found");
     return response()->file($fullPath);
-})->where('path', '.*'); // allow slashes
+})->where('path', '.*'); // allow slashes in path
 
 // Forgot Password routes
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
@@ -20,6 +38,7 @@ Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
     ->middleware('guest')
     ->name('password.email');
 
+// Reset Password routes
 Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
     ->middleware('guest')
     ->name('password.reset');
